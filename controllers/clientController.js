ca25d01fae4beed99ec72b0d6049e475da358423
Anastasia <nnaaastyyyaaa06@gmail.com>
2025-12-10 22:53:45 +0200

    Added complete logic for client

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,6 +1,10 @@
 exports.getAllClients = async (req, res) => {
-  const users = await req.prisma.client.findMany();
-  res.json(users);
+  try {
+    const users = await req.prisma.client.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(505).json({ error: error.message });
+  }
 };
 
 exports.createClient = async (req, res) => {
@@ -12,6 +16,45 @@ exports.createClient = async (req, res) => {
     });
     res.json({ "Created client": user });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(505).json({ error: error.message });
+  }
+};
+
+exports.getClient = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const client = await req.prisma.client.findUnique({
+      where: { client_id: Number(id) },
+    });
+    if (!client) throw new Error("Client not found");
+    res.json(client);
+  } catch (error) {
+    res.status(505).json({ error: error.message });
+  }
+};
+
+exports.updateClient = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedClient = await req.prisma.client.update({
+      where: { client_id: Number(id) },
+      data: req.body,
+    });
+    if (!updatedClient) throw new Error("Failed to update client");
+    res.json({ "Updateed client": updatedClient });
+  } catch (error) {
+    res.status(505).json({ error: error.message });
+  }
+};
+
+exports.deleteClient = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await req.prisma.client.delete({
+      where: { client_id: Number(id) },
+    });
+    res.json({ status: "Deleted successfully!" });
+  } catch (error) {
+    res.status(505).json({ error: error.message });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,3 +5,6 @@ const clientController = require("../controllers/clientController");
 router.get("/", clientController.getAllClients);
 router.post("/client", clientController.createClient);
 module.exports = router;
+router.get("/:id", clientController.getClient);
+router.patch("/update/:id", clientController.updateClient);
+router.delete("/delete/:id", clientController.deleteClient);
