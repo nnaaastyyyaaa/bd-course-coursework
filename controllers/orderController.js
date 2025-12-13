f099d73c3d1a626a056488b5536c9c07b66fa909
Anastasia <nnaaastyyyaaa06@gmail.com>
2025-12-13 16:24:02 +0200

    Added stock updating logic

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,42 @@
+const updateProduct = async (transaction, product_id, quantity) => {
+  const result = await transaction.product.updateMany({
+    where: {
+      product_id: Number(product_id),
+      quantity: { gte: quantity },
+    },
+    data: {
+      quantity: { decrement: quantity },
+    },
+  });
+
+  if (result.count === 0) {
+    throw new Error("Haven't enough products in stock");
+  }
+
+  const updatedProduct = await transaction.product.findUnique({
+    where: { product_id: Number(product_id) },
+  });
+
+  const finalyUpdatedProduct = await transaction.product.update({
+    where: { product_id: Number(product_id) },
+    data: {
+      stock_status: updatedProduct.quantity > 0 ? "in_stock" : "out_of_stock",
+    },
+  });
+
+  return finalyUpdatedProduct;
+};
+
 exports.createOrder = async (req, res) => {
   const { client_id, address, worker_id, items, carrier, payment_method } =
     req.body;
-  const { country, city, street, postal_code } = address;
+
   let newAddress, newOrder, newPayment, newShipment;
-  orderItems = [];
+  let orderItems = [];
   try {
+    const { country, city, street, postal_code } = address;
+    if (!country || !city || !street || !postal_code)
+      throw new Error("Enter full address details");
     if (!items || items.length === 0)
       throw new Error("Order must contain at least one item!");
 
@@ -34,7 +66,17 @@ exports.createOrder = async (req, res) => {
         const { quantity, product_id } = item;
         if (!priceAndId[product_id])
           throw new Error(`Product with id ${product_id} not found`);
-        order_price += quantity * priceAndId[product_id];
+
+        const updatedProduct = await updateProduct(
+          transaction,
+          product_id,
+          quantity
+        );
+
+        if (!updatedProduct)
+          throw new Error("Error while updating product stock");
+
+        order_price += priceAndId[product_id] * quantity;
       }
 
       newOrder = await transaction.orders.create({
@@ -114,12 +156,13 @@ exports.getOrder = async (req, res) => {
   }
 };
 
-exports.updateOrder = async (req, res) => {
+exports.updateOrderStatus = async (req, res) => {
   try {
     const id = req.params.id;
+    const { status } = req.body;
     const updatedOrder = await req.prisma.orders.update({
       where: { order_id: Number(id) },
-      data: req.body,
+      data: { status },
     });
     if (!updatedOrder) throw new Error("Failed to update order");
     res.json({ "Updateed order": updatedOrder });
@@ -144,25 +187,34 @@ exports.addOrderItem = async (req, res) => {
   try {
     const { quantity, order_id, product_id } = req.body;
     let orderItem, newOrder;
-    await req.prisma.$transaction((transaction) => {
-      const product = transaction.product.findUnique({
+    await req.prisma.$transaction(async (transaction) => {
+      const product = await transaction.product.findUnique({
         where: { product_id: Number(product_id) },
       });
       if (!product) throw new Error("Invalid product_id");
 
-      const order = transaction.orders.findUnique({
+      const updatedProduct = await updateProduct(
+        transaction,
+        product_id,
+        quantity
+      );
+
+      if (!updatedProduct)
+        throw new Error("Error while updating product stock");
+
+      const order = await transaction.orders.findUnique({
         where: { order_id: Number(order_id) },
       });
       if (!order) throw new Error("Invalid order_id");
 
-      orderItem = transaction.order_item.create({
+      orderItem = await transaction.order_item.create({
         data: { quantity, order_id, product_id },
       });
       if (!orderItem) throw new Error("Failed to create order_item");
 
       const OIprice = quantity * product.price;
 
-      newOrder = transaction.orders.update({
+      newOrder = await transaction.orders.update({
         where: { order_id: Number(order_id) },
         data: { order_price: order.order_price + OIprice },
       });
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,7 +5,8 @@ const orderController = require("../controllers/orderController");
 router.post("/create", orderController.createOrder);
 router.get("/", orderController.getAllOrders);
 router.get("/:id", orderController.getOrder);
-router.patch("/update/:id", orderController.updateOrder);
+router.patch("/update/:id", orderController.updateOrderStatus);
 router.delete("/delete/:id", orderController.deleteOrder);
+router.post("/add-item", orderController.addOrderItem);
 
 module.exports = router;
